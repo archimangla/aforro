096862fda70f4a723ad70daec8262e687902cdc5
archimangla <archimangla6@gmail.com>
2026-02-04 07:20:28 +0530

    feat: add store inventory listing API

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Inventory
+from .serializers import StoreInventorySerializer
 
-# Create your views here.
+class StoreInventoryView(APIView):
+
+    def get(self, request, store_id):
+        inventory = (
+            Inventory.objects
+            .filter(store_id=store_id)
+            .select_related('product', 'product__category')
+            .order_by('product__title')
+        )
+
+        serializer = StoreInventorySerializer(inventory, many=True)
+        return Response(serializer.data)
